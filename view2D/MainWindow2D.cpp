2839206ad8caeb9a3e5010bfbc06372be8666a92
Yvonne Schütte <Yvonne@83adbce4.funky.uni-osnabrueck.de>
2019-03-19 15:03:27 +0100

    some planet ideas

--- view2D/MainWindow2D.cpp
+++ view2D/MainWindow2D.cpp
@@ -14,6 +14,23 @@ MainWindow2D::MainWindow2D(QWidget* parent) :
 {
     // Setup user interface
     ui->setupUi(this);
+       scene = new QGraphicsScene(this);
+       ui->graphicsView->setScene(scene);
+
+       QBrush greenBrush(Qt::green);
+       QBrush grayBrush(Qt::gray);
+       QBrush redBrush(Qt::red);
+       QPen outlinePenHighlight(Qt::white);
+       outlinePenHighlight.setWidth(2);
+
+       QPen outlinePen(Qt::black);
+       outlinePen.setWidth(0);
+       scene->setBackgroundBrush(Qt::black);
+
+       //Abhängig von Planeten machen
+       for(int i=0;i<20;i++){
+          scene->addEllipse(rand()%100, rand()%100, 20, 20, outlinePen, greenBrush);
+       }
 
 }
 
