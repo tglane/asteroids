4d08ceec663eb218000a07c34df908f32821c8fe
jlippold <jlippold@uos.de>
2019-03-19 12:41:08 +0100

    A little little bit more...

---
+++ datamodel/DataModel.cpp
@@ -0,0 +1,18 @@
+#include "DataModel.hpp"
+
+namespace asteroids{
+
+    DataModel::DataModel(std::string filename)
+    {
+        // TODO initialize Players
+
+        getUniverse(filename);
+
+    }
+
+    DataModel::getUniverse(std::string filename)
+    {
+        // TODO open file, fill map of planets
+    }
+
+}
