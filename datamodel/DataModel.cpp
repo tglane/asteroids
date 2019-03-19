33e83516a50c4d1306411c11cb4c3b8d236e577d
jlippold <jlippold@uos.de>
2019-03-19 13:06:24 +0100

    getUniverse implemented

--- datamodel/DataModel.cpp
+++ datamodel/DataModel.cpp
@@ -12,7 +12,38 @@ namespace asteroids{
 
     DataModel::getUniverse(std::string filename)
     {
-        // TODO open file, fill map of planets
+        m_planets = std::map();
+
+        std::ifstream f;
+        f.open(filename);
+
+        if(f.is_open())
+        {
+            int numvertex, posx, posy, mines;
+            std::string name;
+
+            f >> numvertex;
+
+            // initialize all planets and add them to the map
+            for(int i = 0; i < numvertex; i++)
+            {
+                f >> name >> posx >> posy >> mines;
+                Planet* p = new Planet(name, posx, posy, mines);
+
+                m_planets[name] = p;
+            }
+
+            // add the edges to the map
+            std::string from, to;
+            while(!f.eof())
+            {
+                f >> from >> to;
+                m_planets[from].addNeighbour(m_planets[to]);
+                m_planets[to].addNeighbour(m_planets[from]);
+            }
+
+            f.close();
+        }
     }
 
 }
