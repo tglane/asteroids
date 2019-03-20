6b1b34c67b49be680d43f8cb9e9044bd46604100
jlippold <jlippold@uos.de>
2019-03-20 09:01:36 +0100

    Updated error message

--- Main.cpp
+++ Main.cpp
@@ -14,9 +14,9 @@
 
 int main(int argc, char** argv)
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        cerr << "Usage: asteroids <level-file>" << endl;
+        cerr << "Usage: asteroids <level-file> <map-file>" << endl;
         return 1;
     }
 
