2a6ff581e94c927a72dfc6fcda51a73f73b7ad53
gvogt <gvogt@uos.de>
2019-03-28 10:51:06 +0100

    updated maps and rubinleft

--- datamodel/Planet.hpp
+++ datamodel/Planet.hpp
@@ -37,7 +37,7 @@ namespace asteroids
 		 * @param rubinLeft optional param = 20 how many rubin is left on this planet
 		 *
 		 */
-		Planet(std::string name, int posx, int posy/*, int rubin = 0*/, int mines = 0, int rubinLeft = 20);
+		Planet(std::string name, int posx, int posy/*, int rubin = 0*/, int mines = 0, int rubinLeft = 20000);
 
 		/**
 		 * @brief gives the number of ships on this planet
